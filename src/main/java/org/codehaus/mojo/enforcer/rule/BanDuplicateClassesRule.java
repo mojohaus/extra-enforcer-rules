5d0713f75a4a8561a95382b7833ed41a0ed1e6cf
Stephen Connolly <stephen.alan.connolly@gmail.com>
2011-04-18 21:10:13 +0000

    adding support for regex ignores
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/sandbox/extra-enforcer-rules@13943 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/main/java/org/codehaus/mojo/enforcer/rule/BanDuplicateClassesRule.java
+++ src/main/java/org/codehaus/mojo/enforcer/rule/BanDuplicateClassesRule.java
@@ -2,7 +2,6 @@ package org.codehaus.mojo.enforcer.rule;
 
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.factory.ArtifactFactory;
-import org.apache.maven.artifact.resolver.ArtifactResolver;
 import org.apache.maven.enforcer.rule.api.EnforcerRule;
 import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
 import org.apache.maven.enforcer.rule.api.EnforcerRuleHelper;
@@ -14,20 +13,77 @@ import org.codehaus.plexus.component.repository.exception.ComponentLookupExcepti
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.regex.Pattern;
 
 /**
  * Bans duplicate classes on the classpath.
  */
 public class BanDuplicateClassesRule implements EnforcerRule {
+
+    /**
+     * List of classes to ignore. Wildcard at the end accepted
+     */
+    private String[] ignoreClasses;
+
+    /**
+     * Convert a wildcard into a regex.
+     * @param wildcard the wildcard to convert.
+     * @return the equivalent regex.
+     */
+    private static String asRegex(String wildcard) {
+        StringBuilder result = new StringBuilder(wildcard.length());
+        result.append('^');
+        for (int index = 0; index < wildcard.length(); index++) {
+            char character = wildcard.charAt(index);
+            switch (character) {
+                case '*':
+                    result.append(".*");
+                    break;
+                case '?':
+                    result.append(".");
+                    break;
+                case '$':
+                case '(':
+                case ')':
+                case '.':
+                case '[':
+                case '\\':
+                case ']':
+                case '^':
+                case '{':
+                case '|':
+                case '}':
+                    result.append("\\");
+                default:
+                    result.append(character);
+                    break;
+            }
+        }
+        result.append('$');
+        return result.toString();
+    }
+
+
     public void execute(EnforcerRuleHelper helper) throws EnforcerRuleException {
         Log log = helper.getLog();
+        List<Pattern> ignores = new ArrayList<Pattern>();
+        if (ignoreClasses != null) {
+            for (String ignore : ignoreClasses) {
+                log.info("Adding ignore: " + ignore);
+                ignore = ignore.replace('.', '/');
+                String pattern = asRegex(ignore);
+                log.info("Ignore: " + ignore + " maps to regex " + pattern);
+                ignores.add(Pattern.compile(pattern));
+            }
+        }
         try {
             MavenProject project = (MavenProject) helper.evaluate("${project}");
 
@@ -39,18 +95,28 @@ public class BanDuplicateClassesRule implements EnforcerRule {
                 dependencyArtifacts = project.createArtifacts(factory, null, null);
             }
 
-            Map<String,Artifact> classNames = new HashMap<String,Artifact>();
+            Map<String, Artifact> classNames = new HashMap<String, Artifact>();
             for (Artifact o : dependencyArtifacts) {
                 File file = o.getFile();
                 try {
                     JarFile jar = new JarFile(file);
                     try {
-                        for (JarEntry entry: Collections.<JarEntry>list(jar.entries())) {
+                        outer:
+                        for (JarEntry entry : Collections.<JarEntry>list(jar.entries())) {
                             String name = entry.getName();
-                            if (!name.endsWith(".class")) continue;
+                            if (!name.endsWith(".class")) {
+                                continue;
+                            }
                             Artifact dup = classNames.get(name);
                             if (dup != null) {
-                                throw new EnforcerRuleException("Duplicate class " + name + " found in both " + dup + " and " + o);
+                                for (Pattern p : ignores) {
+                                    if (p.matcher(name).matches()) {
+                                        log.debug("Ignoring duplicate class " + name);
+                                        continue outer;
+                                    }
+                                }
+                                throw new EnforcerRuleException(
+                                        "Duplicate class " + name + " found in both " + dup + " and " + o);
                             }
                             classNames.put(name, o);
                         }
