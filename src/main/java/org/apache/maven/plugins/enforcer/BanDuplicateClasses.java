70d58d6dd7660d541b4edb61d8c64233d847446c
Stephen Connolly <stephen.alan.connolly@gmail.com>
2011-04-19 09:03:14 +0000

    o move the rule to the o.a.m.p.e package so that it can be instantiated more easily o make the negative smoketest pass for true negative not just any negative o add site documentation
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/sandbox/extra-enforcer-rules@13947 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/main/java/org/apache/maven/plugins/enforcer/BanDuplicateClasses.java
+++ src/main/java/org/apache/maven/plugins/enforcer/BanDuplicateClasses.java
@@ -1,4 +1,4 @@
-package org.codehaus.mojo.enforcer.rule;
+package org.apache.maven.plugins.enforcer;
 
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.factory.ArtifactFactory;
@@ -6,6 +6,7 @@ import org.apache.maven.enforcer.rule.api.EnforcerRule;
 import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
 import org.apache.maven.enforcer.rule.api.EnforcerRuleHelper;
 import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.plugins.enforcer.AbstractStandardEnforcerRule;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.artifact.InvalidDependencyVersionException;
 import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluationException;
@@ -29,12 +30,18 @@ import java.util.regex.Pattern;
 /**
  * Bans duplicate classes on the classpath.
  */
-public class BanDuplicateClassesRule implements EnforcerRule {
+public class BanDuplicateClasses extends AbstractStandardEnforcerRule {
 
     /**
      * List of classes to ignore. Wildcard at the end accepted
      */
     private String[] ignoreClasses;
+
+    /**
+     * If {@code false} then the rule will fail at the first duplicate, if {@code true} then the rule will fail at
+     * the end.
+     */
+    private boolean findAllDuplicates;
 
     /**
      * Convert a wildcard into a regex.
@@ -100,17 +107,18 @@ public class BanDuplicateClassesRule implements EnforcerRule {
             }
 
             Map<String, Artifact> classNames = new HashMap<String, Artifact>();
+            Map<String, Set<Artifact>> duplicates = new HashMap<String, Set<Artifact>>();
             for (Artifact o : dependencyArtifacts) {
                 File file = o.getFile();
-                log.info(file.toString());
+                log.debug("Searching for duplicate classes in " + file);
                 if (!file.exists()) {
                     log.warn("Could not find " + o + " at " + file);
                 } else if (file.isDirectory()) {
                     try {
-                    for (String name: (List<String>)FileUtils.getFileNames(file, null, null, false)) {
-                        log.info("  " + name);
-                        checkAndAddName(o, name, classNames, ignores, log);
-                    }
+                        for (String name : (List<String>) FileUtils.getFileNames(file, null, null, false)) {
+                            log.info("  " + name);
+                            checkAndAddName(o, name, classNames, duplicates, ignores, log);
+                        }
                     } catch (IOException e) {
                         throw new EnforcerRuleException(
                                 "Unable to process dependency " + o.toString() + " due to " + e.getLocalizedMessage()
@@ -122,7 +130,7 @@ public class BanDuplicateClassesRule implements EnforcerRule {
                         JarFile jar = new JarFile(file);
                         try {
                             for (JarEntry entry : Collections.<JarEntry>list(jar.entries())) {
-                                checkAndAddName(o, entry.getName(), classNames, ignores, log);
+                                checkAndAddName(o, entry.getName(), classNames, duplicates, ignores, log);
                             }
                         } finally {
                             try {
@@ -139,7 +147,33 @@ public class BanDuplicateClassesRule implements EnforcerRule {
                     }
                 }
             }
-
+            if (!duplicates.isEmpty()) {
+                Map<Set<Artifact>, List<String>> inverted = new HashMap<Set<Artifact>, List<String>>();
+                for (Map.Entry<String, Set<Artifact>> entry : duplicates.entrySet()) {
+                    List<String> s = inverted.get(entry.getValue());
+                    if (s == null) {
+                        s = new ArrayList<String>();
+                    }
+                    s.add(entry.getKey());
+                    inverted.put(entry.getValue(), s);
+                }
+                StringBuilder buf = new StringBuilder(message == null ? "Duplicate classes found:" : message);
+                buf.append('\n');
+                for (Map.Entry<Set<Artifact>, List<String>> entry : inverted.entrySet()) {
+                    buf.append("\n  Found in: ");
+                    for (Artifact a : entry.getKey()) {
+                        buf.append("\n    ");
+                        buf.append(a);
+                    }
+                    buf.append("\n  Duplicate classes:");
+                    for (String className : entry.getValue()) {
+                        buf.append("\n    ");
+                        buf.append(className);
+                    }
+                    buf.append('\n');
+                }
+                throw new EnforcerRuleException(buf.toString());
+            }
 
         } catch (ComponentLookupException e) {
             throw new EnforcerRuleException("Unable to lookup a component " + e.getLocalizedMessage(), e);
@@ -151,7 +185,8 @@ public class BanDuplicateClassesRule implements EnforcerRule {
     }
 
     private void checkAndAddName(Artifact artifact, String name, Map<String, Artifact> classNames,
-                                 Collection<Pattern> ignores, Log log) throws EnforcerRuleException {
+                                 Map<String, Set<Artifact>> duplicates, Collection<Pattern> ignores, Log log)
+            throws EnforcerRuleException {
         if (!name.endsWith(".class")) {
             return;
         }
@@ -163,8 +198,29 @@ public class BanDuplicateClassesRule implements EnforcerRule {
                     return;
                 }
             }
-            throw new EnforcerRuleException("Duplicate class " + name + " found in both " + dup + " and " + artifact)
-                    ;
+            if (findAllDuplicates) {
+                Set<Artifact> dups = duplicates.get(name);
+                if (dups == null) {
+                    dups = new HashSet<Artifact>();
+                }
+                dups.add(artifact);
+                dups.add(dup);
+                duplicates.put(name, dups);
+            } else {
+                StringBuilder buf = new StringBuilder(message == null ? "Duplicate class found:" : message);
+                buf.append('\n');
+                buf.append("\n  Found in: ");
+                buf.append("\n    ");
+                buf.append(dup);
+                buf.append("\n    ");
+                buf.append(artifact);
+                buf.append("\n  Duplicate classes:");
+                buf.append("\n    ");
+                buf.append(name);
+                buf.append('\n');
+                buf.append("There may be others but <findAllDuplicates> was set to false, so failing fast");
+                throw new EnforcerRuleException(buf.toString());
+            }
         }
         classNames.put(name, artifact);
 
