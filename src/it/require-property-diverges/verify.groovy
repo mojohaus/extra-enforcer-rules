19d17915f7e15bce60b5f02c763837491a4c8538
Mirko Friedenhagen <mfriedenhagen@apache.org>
2012-04-19 22:00:11 +0000

    Maven2 does not append a slash to the artifactId when calculating the project.url. This leads to different error message during the integration test and lets the Bamboo build fail.
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/extra-enforcer-rules@16435 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/it/require-property-diverges/verify.groovy
+++ src/it/require-property-diverges/verify.groovy
@@ -5,7 +5,7 @@ assert buf.contains(/org.apache.maven.plugins.enforcer.RequirePropertyDiverges f
 
 assert buf.contains('Property \'project.issueManagement\' is required for this build and not defined in hierarchy at all.');
 
-assert buf.contains('Property \'project.url\' evaluates to \'http://company/company-parent-pom/child-fail/\'');
+assert buf.contains('Property \'project.url\' evaluates to \'http://company/company-parent-pom/child-fail');
 
 assert buf.contains('Property \'project.groupId\' evaluates to \'company.project1\'. This does match \'company.project1\' from parent MavenProject: company.project1:fail-property-without-regex-not-overridden:1.0-SNAPSHOT');
 
