b6a0dde6eb4d50a9032e118d21e0be5e1c577fa4
Martin Goldhahn <mgoldhahn@gmail.com>
2015-11-13 23:14:11 +0100

    Fixes #4: Add enforcer rule for file encoding
    * Checks file encodings to see if they match the project.build.sourceEncoding.
    * If file encoding can not be determined it is skipped.

---
+++ src/it/require-encoding-usascii/verify.groovy
@@ -0,0 +1,12 @@
+File file = new File( basedir, "build.log" );
+assert file.exists();
+
+String text = file.getText("utf-8");
+
+assert text.contains("Encoding US-ASCII is hard to detect.")
+assert text.contains("Files not encoded in US-ASCII:")
+assert text.contains("src/main/resources/utf8.txt==>UTF-8")
+assert text.contains("src/main/resources/ascii.txt==>ISO-8859-1")
+assert text.contains("src/main/resources/iso88591.txt==>ISO-8859-1")
+
+return true;
