1ed3c5ec8ac306884c5c623c79df1fe0ff2971d1
rfscholte <rfscholte@apache.org>
2016-07-23 12:49:38 +0200

    Make verify.groovy OS independent

--- src/it/require-encoding-usascii/verify.groovy
+++ src/it/require-encoding-usascii/verify.groovy
@@ -3,10 +3,10 @@ assert file.exists();
 
 String text = file.getText("utf-8");
 
-assert text.contains("Encoding US-ASCII is hard to detect.")
-assert text.contains("Files not encoded in US-ASCII:")
-assert text.contains("src/main/resources/utf8.txt==>UTF-8")
-assert text.contains("src/main/resources/ascii.txt==>ISO-8859-1")
-assert text.contains("src/main/resources/iso88591.txt==>ISO-8859-1")
+assert text.contains( "Encoding US-ASCII is hard to detect." )
+assert text.contains( "Files not encoded in US-ASCII:" )
+assert text.contains( "src/main/resources/utf8.txt==>UTF-8".replace('/', File.separator) )
+assert text.contains( "src/main/resources/ascii.txt==>ISO-8859-1".replace('/', File.separator) )
+assert text.contains( "src/main/resources/iso88591.txt==>ISO-8859-1".replace('/', File.separator) )
 
 return true;
