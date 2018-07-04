5b743c4202c002784c0365b1a1571fee0d215592
Adam Voss <adam.voss@cerner.com>
2018-07-04 10:13:46 -0500

    Add test showing Issue #57 is fixed

---
+++ src/it/enforce-bytecode-version-jdkVersionOption-runtime-scope/verify.groovy
@@ -0,0 +1,14 @@
+File file = new File( basedir, "build.log" );
+assert file.exists();
+
+String text = file.getText("utf-8");
+
+assert text.contains("Found Banned Dependency: org.hibernate:hibernate-annotations:jar:3.4.0.GA")
+assert text.contains("Found Banned Dependency: org.hibernate:ejb3-persistence:jar:1.0.2.GA")
+assert text.contains("Found Banned Dependency: org.hibernate:hibernate-commons-annotations:jar:3.1.0.GA")
+assert text.contains("Found Banned Dependency: org.hibernate:hibernate-core:jar:3.3.0.SP1")
+assert text.contains("Found Banned Dependency: javax.transaction:jta:jar:1.1")
+assert text.contains("Found Banned Dependency: org.slf4j:slf4j-api:jar:1.4.2")
+assert text.contains("Found Banned Dependency: dom4j:dom4j:jar:1.6.1")
+
+return true;
