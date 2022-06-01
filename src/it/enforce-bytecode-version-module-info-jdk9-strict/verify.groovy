58cc452cb6ff43530296d97db9786ad33c2aa3ab
Clement Cherlin <ccherlin@dminc.com>
2022-06-01 14:15:56 -0500

    Add strict option to EnforceBytecodeVersion to not ignore module-info and Multi-Release JAR classes

---
+++ src/it/enforce-bytecode-version-module-info-jdk9-strict/verify.groovy
@@ -0,0 +1,9 @@
+File file = new File( basedir, "build.log" );
+assert file.exists();
+
+String text = file.getText("utf-8");
+
+assert ! text.contains( '[INFO] Adding ignore: module-info' )
+assert ! text.contains( 'Found Banned Dependency: org.ow2.asm:asm:jar:6.0' )
+
+return true;
