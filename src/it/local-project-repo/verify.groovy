b9965e4db53a358faad04b78ee98adb0309d6854
Slawomir Jaranowski <s.jaranowski@gmail.com>
2022-11-23 20:18:01 +0100

    Use Resolver Api for resolving artifacts
    fix #231

---
+++ src/it/local-project-repo/verify.groovy
@@ -0,0 +1,8 @@
+File file = new File( basedir, "build.log" );
+assert file.exists();
+
+String text = file.getText( "utf-8" );
+
+assert text.contains( '[DEBUG] Analyzing artifact dumy:dumy-local-repo:pom:1.0:compile' )
+
+return true;
